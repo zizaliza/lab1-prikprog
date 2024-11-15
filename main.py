ff6a604ba35fe68c81c106916beb6b8c9b3331aa
zizaliza <lisakruzh@gmail.com>
2024-11-15 19:46:13 +0300

    Добавлены дочерние классы классы к базовому классу Movie

--- main.py
+++ main.py
@@ -1,13 +1,89 @@
 class Movie:
     def __init__(self, name: str, duration: int, year: int):
-        self.name = name
-        self.duration = duration
-        self.year = year
+        self.__name = name
+        self.__duration = duration
+        self.__year = year
+        
+    @property
+    def name(self):
+        return self.__name
 
-    def to_dict(self):
-        return {
-            "name": self.name,
-            "duration": self.duration,
-            "year": self.year
+    @name.setter
+    def name(self, name: str):
+        self.__name = name
 
-        }
+    @property
+    def duration(self):
+        return self.__duration
+
+    @duration.setter
+    def duration(self, duration: int):
+        self.__duration = duration
+
+    @property
+    def year(self):
+        return self.__year
+
+    @year.setter
+    def year(self, year: int):
+        self.__year = year
+        
+    def get_genre(self):
+        pass
+
+class Comedy(Movie):
+    def get_genre(self):
+        return "Жанр - комедия"
+    
+class Horror(Movie):
+    def get_genre(self):
+        return "Жанр - ужасы"
+    
+class Drama(Movie):
+    def get_genre(self):
+        return "Жанр - драма"
+    
+class Fantasy(Movie):
+    def get_genre(self):
+        return "Жанр - фантастика"
+    
+class Detective(Movie):
+    def get_genre(self):
+        return "Жанр - детектив"
+    
+class Melodrama(Movie):
+    def get_genre(self):
+        return "Жанр - мелодрама"
+    
+class Documentary(Movie):
+    def get_genre(self):
+        return "Жанр - документальный"
+    
+class Cartoons(Movie):
+    def get_genre(self):
+        return "Жанр - мультики"
+    
+class Anime(Movie):
+    def get_genre(self):
+        return "Жанр - аниме"
+    
+class ActionMovie(Movie):
+    def get_genre(self):
+        return "Жанр - боевик"
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+
+    
+    
+
+        
+
