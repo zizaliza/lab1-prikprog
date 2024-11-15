c8961c260a3c76e5fbb821b1ee46f1e23dc74daa
zizaliza <lisakruzh@gmail.com>
2024-11-15 19:23:41 +0300

    Добавлен базовый класс Movie

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hi") 
+class Movie:
+    def __init__(self, name: str, duration: int, year: int):
+        self.name = name
+        self.duration = duration
+        self.year = year
+
+    def to_dict(self):
+        return {
+            "name": self.name,
+            "duration": self.duration,
+            "year": self.year
+
+        }
